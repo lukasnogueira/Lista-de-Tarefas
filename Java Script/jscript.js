2d7f610b76e0b5a94763596c8699789ebb9f910b
lukasnogueira <79331207+lukasnogueira@users.noreply.github.com>
2022-07-02 08:23:34 -0300

    Esborso do html/css, inicio do processo js.

---
+++ Java Script/jscript.js
@@ -0,0 +1,9 @@
+function add() {
+    var add = document.querySelector('.inputs')
+
+    var add1 = document.createElement('input')
+    add1.type = 'Text'
+    add1.placeholder = 'Tarefa'
+    add1.class = 'tarefa'
+    add.appendChild(add1);
+}
